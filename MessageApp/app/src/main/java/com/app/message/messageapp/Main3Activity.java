bec21fbd777e2a7cce644bb82aaaaea454744099
Curtis Murphy <murphy.curtis12@gmail.com>
2017-11-22 15:04:00 +0000

    Third Version of App with second Chat Room made

--- MessageApp/app/src/main/java/com/app/message/messageapp/Main3Activity.java
+++ MessageApp/app/src/main/java/com/app/message/messageapp/Main3Activity.java
@@ -1,17 +1,15 @@
 package com.app.message.messageapp;
 
 import android.content.Intent;
-import android.graphics.Bitmap;
+import android.os.Handler;
 import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.ImageView;
-import android.widget.Toast;
 
-public class Main2Activity extends AppCompatActivity {
+public class Main3Activity extends AppCompatActivity {
 
     private static final int REQUEST_CODE_IMAGE_CAPTURE = 100;
     ImageView ivImage;
@@ -19,10 +17,10 @@ public class Main2Activity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main2);
+        setContentView(R.layout.activity_main3);
 
-        ImageButton cameraBtn = (ImageButton) findViewById(R.id.goToCameraBtn);
-        cameraBtn.setOnClickListener(new View.OnClickListener() {
+        ImageButton cameraBtnTwo = (ImageButton) findViewById(R.id.goToCameraBtnTwo);
+        cameraBtnTwo.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 captureImage();
@@ -30,7 +28,9 @@ public class Main2Activity extends AppCompatActivity {
                 startActivityForResult(iCamera, REQUEST_CODE_IMAGE_CAPTURE);
             }
         });
+
     }
+
     private void captureImage() {
         cameraImplicitIntent();
     }
@@ -39,14 +39,4 @@ public class Main2Activity extends AppCompatActivity {
         Intent iCamera = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
         startActivityForResult(iCamera, REQUEST_CODE_IMAGE_CAPTURE);
     }
-
-    /*@Override
-    protected void onACctivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        if(requestCode == REQUEST_CODE_IMAGE_CAPTURE && resultCode == RESULT_OK) {
-            Toast.makeText(getApplicationContext(), "Result Back from Camera App", Toast.LENGTH_SHORT).show();
-            Bundle bundle = data.getExtras();
-            Bitmap bitmapImage = (Bitmap) bundle.get("data");
-        }
-    }*/
 }
--- MessageApp/app/src/main/java/com/app/message/messageapp/MainActivity.java
+++ MessageApp/app/src/main/java/com/app/message/messageapp/MainActivity.java
@@ -1,13 +1,39 @@
 package com.app.message.messageapp;
 
+import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        Button chatRoom = (Button)findViewById(R.id.chatButton);
+        Button chatRoomTwo = (Button)findViewById(R.id.chatButtonTwo);
+
+        chatRoom.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(),Main2Activity.class);
+                startActivity(intent);
+            }
+        });
+
+        chatRoomTwo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent (getApplicationContext(),Main3Activity.class);
+                startActivity(intent);
+            }
+        });
     }
+
 }
